33b66e584d7662fcac07f9489ea63715387fc351
mscalel <msksaul@gmail.com>
2022-09-09 15:44:06 -0600

    Update Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 import { useDispatch } from 'react-redux'
@@ -13,12 +13,13 @@ import memories from './assets/memories.png'
 import useStyles from './styles'
 
 function App() {
+  const [currentId, setCurrentId] = useState(null)
   const classes = useStyles()
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(getPosts())
-  }, [dispatch])
+  }, [currentId, dispatch])
 
   return (
     <Container maxWidth='lg'>
@@ -30,10 +31,10 @@ function App() {
         <Container>
           <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
             <Grid item xs={12} sm={7}>
-              <Posts />
+              <Posts setCurrentId={setCurrentId}/>
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Form />
+              <Form currentId={currentId} setCurrentId={setCurrentId}/>
             </Grid>
           </Grid>
         </Container>
